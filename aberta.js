002fe6d99349ba768b078d82022ad23aacadd69b
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2023-03-17 15:44:34 -0300

    first commit

---
+++ aberta.js
@@ -0,0 +1,14 @@
+/*
+
+"Qual a sua análise do impacto social que a IA trará para as pessoas e empresas no futuro próximo?"
+
+--> Resposta: 
+
+Acredito que a IA oferece muitos benefícios para os mais diversos segmentos, podendo melhorar a eficiência operacional e até mesmo a produtividade.
+Entretanto, a forma de incluir conhecimento a ela é através da inserção de dados, o que pode ocasionar uma limitação importante, visto que camadas
+adicionais devem ser adicionadas separadamente e qualquer falha pode refletir nos resultados. Talvez por isso, grande parte dos sistemas atualmente,
+são treinados para fazer tarefas mais específicas. Dessa forma, acredito que ela continuará sendo um grande apoio para as pessoas, expandindo as suas
+capacidades e proporcionando melhorias na execução de várias tarefas, mas não substituindo o ser humano, principalmente em atividades que requerem
+criatividade.
+
+*/
